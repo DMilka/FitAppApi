e55fe06d1f9022757b2cd280c355c554b7c5aaa9
Dawid Milka <dawid.milka@interia.com>
2021-10-12 00:15:13 +0200

    [development] Added ingredient's entity along with its type

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IngredientRepository::class)
+ */
+class Ingredient
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $description = null;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private ?float $amount = null;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $protein = null;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $carbohydrate = null;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $fat = null;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $calorie = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Users::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private int $userId;
+
+    /**
+     * @ORM\OneToMany(targetEntity=AmountType::class, mappedBy="ingredient", orphanRemoval=true)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private int $amountTypeId;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getProtein(): ?float
+    {
+        return $this->protein;
+    }
+
+    public function setProtein(?float $protein): self
+    {
+        $this->protein = $protein;
+
+        return $this;
+    }
+
+    public function getCarbohydrate(): ?float
+    {
+        return $this->carbohydrate;
+    }
+
+    public function setCarbohydrate(?float $carbohydrate): self
+    {
+        $this->carbohydrate = $carbohydrate;
+
+        return $this;
+    }
+
+    public function getFat(): ?float
+    {
+        return $this->fat;
+    }
+
+    public function setFat(?float $fat): self
+    {
+        $this->fat = $fat;
+
+        return $this;
+    }
+
+    public function getCalorie(): ?float
+    {
+        return $this->calorie;
+    }
+
+    public function setCalorie(?float $calorie): self
+    {
+        $this->calorie = $calorie;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
