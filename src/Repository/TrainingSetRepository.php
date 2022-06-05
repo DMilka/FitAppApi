2e4012d90467207ec75f38c36d88cb076f09e6f1
Dawid Milka <dawid.milka@interia.com>
2022-06-05 15:29:00 +0200

    [development] Typo fix

--- src/Repository/TrainingSetRepository.php
+++ src/Repository/TrainingSetRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\AmountType;
 use App\Entity\Training;
+use App\Entity\TrainingSet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,6 +12,6 @@ class TrainingSetRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Trai::class);
+        parent::__construct($registry, TrainingSet::class);
     }
 }
